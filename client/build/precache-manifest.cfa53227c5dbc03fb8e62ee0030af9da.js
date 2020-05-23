050f3d9d1c424c94e06d475d375eb5f6dc1875c2
Gary Llil <garyjllil@gmail.com>
2020-05-23 19:51:20 -0400

    ran build

---
+++ client/build/precache-manifest.cfa53227c5dbc03fb8e62ee0030af9da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4773d23c6042a90dc78c0104747d75cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1a39700970873901798",
+    "url": "/static/css/main.f72a47cc.chunk.css"
+  },
+  {
+    "revision": "7517391621e3cefc4275",
+    "url": "/static/js/2.807b2aef.chunk.js"
+  },
+  {
+    "revision": "8debe1713c30c1c8fa79b57451984727",
+    "url": "/static/js/2.807b2aef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a39700970873901798",
+    "url": "/static/js/main.a5e1eba0.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
