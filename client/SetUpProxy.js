f102c70ef91e4832dfd3b6738b48a24f21cb8208
Gary Llil <garyjllil@gmail.com>
2020-05-25 07:32:57 -0400

    redid scripts

--- client/SetUpProxy.js
+++ client/SetUpProxy.js
@@ -1,8 +1,8 @@
 const proxy = require("http-proxy-middleware");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 module.exports = function(app) {
 
-    app.use(proxy("/api", { target: "http://localhost:" + PORT }))
+    app.use(proxy("/api/sendMail", { target: "http://localhost:" + PORT }))
 }
