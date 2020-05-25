ff6d8836cd7ca874adf6731e478453b84c3efbd6
Gary Llil <garyjllil@gmail.com>
2020-05-25 08:07:08 -0400

    corrected path

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
+const path = require("path");
 
 const API = require("./routes/API");
 
@@ -18,7 +19,7 @@ app.use("/api/sendMail", API);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
